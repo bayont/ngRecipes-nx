6f453605f7811f7a448593afa265ae99da6c189e
bayont <f.fetter02@gmail.com>
2023-03-15 11:44:39 +0100

    chore(recipes): cover recipes-list library with unit tests

--- libs/recipes/feature-recipes-list/src/lib/recipes-list-item/recipes-list-item.component.spec.ts
+++ libs/recipes/feature-recipes-list/src/lib/recipes-list-item/recipes-list-item.component.spec.ts
@@ -1,13 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterTestingModule } from '@angular/router/testing';
+import { recipesFixtures } from '@ngrecipes-nx/recipes/data-access-recipes';
 
 import { RecipesListItemComponent } from './recipes-list-item.component';
 
 describe('RecipesListItemComponent', () => {
   let component: RecipesListItemComponent;
   let fixture: ComponentFixture<RecipesListItemComponent>;
+  const recipeFixture = recipesFixtures[0];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [MatIconModule, RouterTestingModule],
       declarations: [RecipesListItemComponent],
     }).compileComponents();
 
@@ -18,5 +23,12 @@ describe('RecipesListItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit delete recipe event', () => {
+    component.value = recipeFixture;
+    jest.spyOn(component.deleteRecipe, 'emit');
+    component.onDeleteRecipe();
+    expect(component.deleteRecipe.emit).toBeCalledWith(recipeFixture);
   });
 });
