106e4941d893edf2caec899a8e9d23744c1af1fd
bayont <f.fetter02@gmail.com>
2023-03-16 15:10:33 +0100

    chore(recipes): cover interceptors and http service with unit tests

--- libs/recipes/data-access-recipes/src/lib/interceptors/http-error.interceptor.spec.ts
+++ libs/recipes/data-access-recipes/src/lib/interceptors/http-error.interceptor.spec.ts
@@ -1,17 +1,56 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  MatSnackBar,
+  MatSnackBarRef,
+  TextOnlySnackBar,
+} from '@angular/material/snack-bar';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { HttpErrorInterceptor } from './http-error.interceptor';
+import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 describe('HttpErrorInterceptor', () => {
-  beforeEach(() =>
+  let matSnackBar: MatSnackBar;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  const mockMatSnackBar = {
+    open: () => {
+      return {} as MatSnackBarRef<TextOnlySnackBar>;
+    },
+  };
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpErrorInterceptor],
-    })
-  );
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: HttpErrorInterceptor,
+          multi: true,
+        },
+        { provide: MatSnackBar, useValue: mockMatSnackBar },
+      ],
+    });
+    matSnackBar = TestBed.inject(MatSnackBar);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
 
-  it('should be created', () => {
-    const interceptor: HttpErrorInterceptor =
-      TestBed.inject(HttpErrorInterceptor);
-    expect(interceptor).toBeTruthy();
+  it('should display matSnackBar when status code is 400', () => {
+    jest.spyOn(matSnackBar, 'open');
+    httpClient.get('/test').subscribe();
+    const req = httpTestingController.expectOne('/test');
+    req.flush('API key expired', {
+      status: 400,
+      statusText: 'API key expired',
+    });
+    httpTestingController.verify();
+
+    expect(matSnackBar.open).toBeCalledWith(
+      'CrudCrud API key expired',
+      'CLOSE'
+    );
   });
 });
--- libs/recipes/data-access-recipes/src/lib/services/recipe-http.service.spec.ts
+++ libs/recipes/data-access-recipes/src/lib/services/recipe-http.service.spec.ts
@@ -1,16 +1,82 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Recipe } from '../models';
 
 import { RecipeHttpService } from './recipe-http.service';
 
 describe('RecipeHttpService', () => {
   let service: RecipeHttpService;
+  const mockHttpClient = {
+    get: jest.fn(() => of()),
+    post: jest.fn(() => of()),
+    put: jest.fn(() => of()),
+    delete: jest.fn(() => of()),
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: HttpClient, useValue: mockHttpClient }],
+    });
     service = TestBed.inject(RecipeHttpService);
+    jest.clearAllMocks();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('.fetchRecipes()', () => {
+    it('should call http.get with proper recipe endpoint', () => {
+      service.fetchRecipes();
+      expect(mockHttpClient.get).toBeCalledWith(
+        expect.stringMatching(/https:\/\/crudcrud.com\/api\//)
+      );
+    });
+  });
+
+  describe('.fetchRecipe(recipeId)', () => {
+    const recipeId = 'randomId';
+    it('should call http.get with specific recipeId', () => {
+      service.fetchRecipe(recipeId);
+      expect(mockHttpClient.get).toBeCalledWith(
+        expect.stringMatching(
+          new RegExp(`https://crudcrud.com/api/.+/${recipeId}`)
+        )
+      );
+    });
+  });
+
+  describe('.updateRecipe(recipe)', () => {
+    const recipe: Recipe = {
+      _id: 'pizza1',
+      name: 'Updated Pizza',
+      description: 'description',
+      ingredients: [],
+      preparationTimeInMinutes: 30,
+    };
+
+    it('should call http.put with given recipe as body', () => {
+      service.updateRecipe(recipe);
+      const { _id, ...recipeWithStrippedId } = recipe;
+      expect(mockHttpClient.put).toBeCalledWith(
+        expect.stringMatching(new RegExp(`https://crudcrud.com/api/.+/${_id}`)),
+        recipeWithStrippedId,
+        { observe: 'response' }
+      );
+    });
+  });
+
+  describe('.createRecipe(recipe)', () => {
+    const recipe: Omit<Recipe, '_id'> = {
+      name: 'Pizza',
+      description: 'description',
+      ingredients: [],
+      preparationTimeInMinutes: 30,
+    };
+
+    it('should call http.post with given recipe as body', () => {
+      service.createRecipe(recipe);
+      expect(mockHttpClient.post).toBeCalledWith(
+        expect.stringMatching(/https:\/\/crudcrud.com\/api\//),
+        recipe
+      );
+    });
   });
 });
