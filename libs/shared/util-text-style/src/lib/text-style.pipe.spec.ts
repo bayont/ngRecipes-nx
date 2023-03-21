996bb1dc66f6c92492267948812571ad399c665c
bayont <f.fetter02@gmail.com>
2023-03-21 12:59:33 +0100

    test(shared): use ngneat/spectator and ng-mocks

--- libs/shared/util-text-style/src/lib/text-style.pipe.spec.ts
+++ libs/shared/util-text-style/src/lib/text-style.pipe.spec.ts
@@ -1,32 +1,35 @@
-import { TextStylePipe } from './text-style.pipe';
-import { TestBed } from '@angular/core/testing';
 import { DomSanitizer } from '@angular/platform-browser';
+import { createPipeFactory, mockProvider } from '@ngneat/spectator/jest';
+import { TextStylePipe } from './text-style.pipe';
 
 describe('TextStylePipe', () => {
-  const mockDomSanitizer = {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
-    bypassSecurityTrustHtml: (value: string) => {},
+  const createPipe = createPipeFactory(TextStylePipe);
+  const testSetup = (template: string) => {
+    const spectator = createPipe(template, {
+      providers: [
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        mockProvider(DomSanitizer, {
+          bypassSecurityTrustHtml: jest.fn((value) => value),
+        }),
+      ],
+    });
+    const domSanitizer = spectator.inject(DomSanitizer);
+
+    return { spectator, domSanitizer };
   };
-  let domSanitizer: DomSanitizer;
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TextStylePipe],
-      providers: [{ provide: DomSanitizer, useValue: mockDomSanitizer }],
-    }).compileComponents();
-    domSanitizer = TestBed.inject(DomSanitizer);
-  });
 
   it('create an instance', () => {
-    const pipe = new TextStylePipe(domSanitizer);
-    expect(pipe).toBeTruthy();
+    const { spectator } = testSetup('{{ "text" | textStyle }}');
+    expect(spectator.element).toBeTruthy();
   });
 
   it('should render highlighted text', () => {
-    const text = 'This *word* is highlighted';
-    const textStylePipe = new TextStylePipe(domSanitizer);
-    jest.spyOn(domSanitizer, 'bypassSecurityTrustHtml');
-
-    textStylePipe.transform(text);
+    const { spectator, domSanitizer } = testSetup(
+      '{{ "This *word* is highlighted" | textStyle }}'
+    );
+    expect(spectator.element).toHaveText(
+      `This <span class='highlight-text'>word</span> is highlighted`
+    );
     expect(domSanitizer.bypassSecurityTrustHtml).toBeCalledWith(
       `This <span class='highlight-text'>word</span> is highlighted`
     );
